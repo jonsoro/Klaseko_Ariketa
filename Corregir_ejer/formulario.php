c191a0c42c38b19bff088a1cacc630e41bfa2df2
root <root@zubiri-VirtualBox.(none)>
2014-09-17 12:14:17 +0200

    Segundo

---
+++ Corregir_ejer/formulario.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<?php
+// No funciona cuando se envia un valor no numerico!!!
+$num = 0;
+if(isset($_GET["num"])){
+$num = $_GET["num"];
+} else {
+$num = 1;
+}
+?>
+<br>
+<table border="1">
+<?php
+for ($x=0;$x<$num;$x++) {
+echo "<tr>
+<td>Jill</td>
+<td>Smith</td>
+</tr>";
+}
+?>
+</table>
+</body>
+</html>
