f11cbcaa21979bea0e33c03b0c2fd896f542f05e
root <root@zubiri-VirtualBox.(none)>
2014-09-17 12:03:15 +0200

    primero

---
+++ ejercicio-tabla/tabla.php
@@ -0,0 +1,18 @@
+<html>
+<title>Tabla </title>
+<body>
+	<?php
+		
+		echo "<table border =1>";
+		for  ($i=1; $i<=$_GET["num"]; $i++){
+			
+			echo "<tr> 
+						<td>KK</td> 
+					   	<td>KK</td> 
+				  </tr>";
+			
+		}
+		echo "</table>";
+	?>
+</body>
+</html>
